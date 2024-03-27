bb98d8c2666fe1246cc83716a52b590739b35e18
isaque silva <isaque.silva@equilibriumweb.com>
2024-03-27 10:13:28 -0300

    update dados testes

---
+++ dado-sql-testes/produtos_202403271010.sql
@@ -0,0 +1,22 @@
+INSERT INTO manager_financial.produtos (preco,descricao,tipo,percentual) VALUES
+	 (5000,'iphone 14','A',7),
+	 (7000,'iphone 14 pro','AA',10),
+	 (8000,'iphone 14 pro max','AAA',17),
+	 (101,'iPhone 15 Pro','A',7),
+	 (3000,'iPhone 15 Pro Max','AA',10),
+	 (231,'iPhone 15','AA',10),
+	 (321,'iPhone 15 Plus','AA',10),
+	 (543,'iPhone 14 Pro ','A',7),
+	 (764,'iPhone 14 Pro Max','AAA',17),
+	 (32,'beta postman','AAA',17);
+INSERT INTO manager_financial.produtos (preco,descricao,tipo,percentual) VALUES
+	 (32,'beta postman','AAA',17),
+	 (32,'beta postman','AAA',17),
+	 (32,'beta postman','AAA',17),
+	 (43,'iPhone SE ','A',7),
+	 (431,'iPhone 13 Pro','A',7),
+	 (543,'iPhone 13 Pro Max','A',7),
+	 (322,'iPhone 13','A',7),
+	 (34,'iPhone 12 Pro ','A',7),
+	 (765,'iPhone 12 Pro Max','AA',10),
+	 (631,'iPhone 12','A',7);
---
+++ dado-sql-testes/vendas_202403271008.sql
@@ -0,0 +1,10 @@
+INSERT INTO manager_financial.vendas (produto_id,numero_venda,quantidade_produto) VALUES
+	 (2,1,2),
+	 (1,1,6),
+	 (3,2,1),
+	 (2,2,1),
+	 (19,3,1),
+	 (18,3,1),
+	 (20,4,1),
+	 (19,4,1),
+	 (18,4,1);
